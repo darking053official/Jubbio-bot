b86d50f9f65d554572f6e569ee1098c1155bc89e
Darking053 <darking053@protonmail.com>
2026-03-16 11:59:28 +0300

    Update pause.js

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -1,19 +1,16 @@
-const config = require('../../config');
-
 module.exports = {
   name: 'pause',
   description: 'Müziği duraklatır',
-  aliases: ['durdur', 'bekle'],
-  cooldown: 3,
+  aliases: ['durdur'],
   
   async execute(message, args, client) {
-    const voiceChannel = message.member?.voice?.channel;
-    if (!voiceChannel) return message.reply(config.messages.notInVoiceChannel);
+    const serverQueue = client.queue.get(message.guild.id);
+    
+    if (!serverQueue || !serverQueue.songs.length) {
+      return message.reply('❌ **Çalan müzik yok!**');
+    }
 
-    const serverQueue = client.queue?.get(message.guild.id);
-    if (!serverQueue) return message.reply(config.messages.noMusicPlaying);
-
-    serverQueue.player.pause();
-    return message.reply(config.messages.paused);
+    serverQueue.player?.pause();
+    await message.reply('⏸️ **Duraklatıldı!**');
   }
 };
